49a6f4c99deb9e9110ad4ee14db948c9e3b563d0
preeti1205 <preeti.bhandari1205@gmail.com>
2016-12-29 19:14:40 -0500

    median of two sorted arrays

--- scripts/game.js
+++ scripts/game.js
@@ -4,8 +4,20 @@ var game = function(grid) {
 
   for( var i = 0; i < r; i++) {
     for (var j = 0; j < c; j++) {
-      
+      var sum = findSum( i, j, grid, r, c);
+      if (sum == 3)
+      if (sum == 4)
     }
   }
+}
 
+var findSum = function( i, j, grid, r, c) {
+  var sum = 0;
+  for(var x = i - 1; x <= i + 1; x++) {
+    for(var y = j - 1; y <= j + 1; y++) {
+      if (x >= 0 && x < r && y >= 0; y < c)
+          sum += grid[i][j];
+    }
+  }
+  return sum;
 }
